5789782c9b198a5c70182fdc4bd3b171471fbdf9
zentooo <ankerasoy@gmail.com>
2012-02-04 22:20:17 +0900

    initial commit

---
+++ .dim.pl
@@ -0,0 +1,7 @@
+# This file is managed by /home/yokoe/perl5/perlbrew/perls/perl-5.12.4/bin/dim.
+{
+  dist => 'Amon2::Setup::Flavor::Zentooo',
+  distdir => 'Amon2-Setup-Flavor-Zentooo',
+  module => 'Amon2::Setup::Flavor::Zentooo',
+  template => 'Default'
+}
---
+++ t/001_basic.t
@@ -0,0 +1,10 @@
+#!perl -w
+use strict;
+use Test::More;
+
+use Amon2::Setup::Flavor::Zentooo;
+
+# test Amon2::Setup::Flavor::Zentooo here
+pass;
+
+done_testing;
