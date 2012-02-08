d988f4022557c8e7c51c81dfaed756a394f78be3
zentooo <ankerasoy@gmail.com>
2012-02-08 16:19:49 +0900

    not to extract routes from controllers

--- lib/Amon2/Setup/Flavor/Zentooo.pm
+++ lib/Amon2/Setup/Flavor/Zentooo.pm
@@ -265,32 +265,6 @@ my $router = router {
     # connect '/' => {controller => 'Root', action => 'index' };
 };
 
-my @controllers = Module::Pluggable::Object->new(
-    require     => 1,
-    search_path => ['<% $module %>::<% $moniker %>::C'],
-)->plugins;
-{
-    no strict 'refs';
-    for my $controller (@controllers) {
-        my $p0 = $controller;
-        $p0 =~ s/^<% $module %>::<% $moniker %>::C:://;
-        my $p1 = $p0 eq 'Root' ? '' : decamelize($p0) . '/';
-
-        for my $method (sort keys %{"${controller}::"}) {
-            next if $method =~ /(?:^_|^BEGIN$|^import$)/;
-            my $code = *{"${controller}::${method}"}{CODE};
-            next unless $code;
-            next if get_code_package($code) ne $controller;
-            my $p2 = $method eq 'index' ? '' : $method;
-            my $path = "/$p1$p2";
-            $router->connect($path => {
-                controller => $p0,
-                action     => $method,
-            });
-            print STDERR "map: $path => ${p0}::${method}\n" unless $ENV{HARNESS_ACTIVE};
-        }
-    }
-}
 
 sub dispatch {
     my ($class, $c) = @_;
